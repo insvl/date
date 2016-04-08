56e650d99485fad5b7ba7a34cbe3ed812a3f1ffd
insvl <ivazql00@estudiantes.unileon.es>
2016-04-08 20:24:49 +0200

    Update Date.java

--- Date.java
+++ Date.java
@@ -1,19 +1,22 @@
+public class Main {
 
-public class Date {
-	
-	//los atributos siempre van en private y no se inicializan
-	
-	private int _dia;
-	private int _mes;
-	private int _agno;
-	
-	public Date(int dia, int mes, int agno){ 
-		compruebaDia(dia, mes);
-		_dia = dia;
-		_mes = mes;
-		_agno = agno;
+	public static void main(String[] args) {
+		Date fecha = new Date (5,5,17);
+		fecha.restoMeses();
+		//System.out.println(fecha.restoMeses);
+		
+		fecha.restoDiasMes();
+		
+		
+		
+		
+		//Date fecha2 = new Date (5, 5, 17);
+		//System.out.println(fecha.get_mes());
+		//fecha.isSameDay(fecha2);
+		
+		
 	}
-
+	
 	public void compruebaDia(int dia, int mes){
 		if(mes == 1 && dia < 0 || dia > 31){
 			System.out.println("Error en enero.");
@@ -50,62 +53,7 @@ public class Date {
 		}
 		if(mes == 12 && dia < 0 || dia > 31){
 			System.out.println("Error en diciembre.");
-		}
-		
-		
-		
-	}
-	
-	public int get_dia() {
-		return _dia;
+		}	
 	}
 
-	public void set_dia(int _dia) {
-		this._dia = _dia;
-	}
-
-	public String get_mes() {
-		String mes="";
-		switch(_mes)
-		{
-		case 1: mes = "enero";
-			break;
-		case 2: mes = "febrero";
-			break;
-		case 3: mes = "marzo";
-			break;
-		case 4: mes = "abril";
-			break;
-		case 5: mes = "mayo";
-			break;
-		case 6: mes = "junio";
-			break;
-		case 7: mes = "julio";
-			break;
-		case 8: mes = "agosto";
-			break;
-		case 9: mes = "septiembre";
-			break;
-		case 10: mes = "octubre";
-			break;
-		case 11: mes = "noviembre";
-			break;
-		case 12: mes = "diciembre";
-			break;
-		} 
-		return mes;
-	}
-
-	public void set_mes(int _mes) {
-		this._mes = _mes;
-	}
-
-	public int get_agno() {
-		return _agno;
-	}
-
-	public void set_agno(int _agno) {
-		this._agno = _agno;
-	}
 }
-
